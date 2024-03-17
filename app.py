651a3e9e917eaab3854dea00ba52235b494c8914
Your Name <you@example.com>
2024-03-17 16:01:47 +0900

    clone

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ import csv
 import streamlit as st
 from google.cloud import vision
 from google.oauth2 import service_account
+from openai import OpenAI
+
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "secret.json"
+
+# # Vision APIのクライアントを初期化
+# client = vision.ImageAnnotatorClient()
 
 credentials_info = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
 
@@ -15,6 +21,28 @@ credentials = service_account.Credentials.from_service_account_info(credentials_
 
 # Credentialsを使ってVision APIのクライアントを初期化
 client = vision.ImageAnnotatorClient(credentials=credentials)
+
+st.title("どうさん")
+
+API_KEY = st.sidebar.text_input(
+    "APIキーを入力してください"
+)
+
+if not API_KEY:
+    st.write("APIキーを入力してください")
+    st.stop()
+
+models  = {
+    'gpt-3.5': "gpt-3.5-turbo",
+    'gpt-4': "gpt-4-1106-preview"
+}
+
+model_num = st.sidebar.selectbox(
+    'モデルの選択',
+    ('gpt-3.5', 'gpt-4')
+)
+
+model = models[model_num]
 
 pattern_dict = {}
 pattern_dict["serial"]   = 'Serial #:.*'
@@ -50,11 +78,11 @@ def get_matched_string(pattern, string):
 # csv_file_path = 'ocr.csv'
 
 #with open(csv_file_path, 'a', newline='') as csv_file:
-st.title("OCR")
 
 def vision_img(input_file):
     header = ['folder_name', 'excerpt_0', 'excerpt_1', 'excerpt_2', 'pulverization_0', 'pulverization_1', 'pulverization_2', 'label_by_human']
     row = 0
+    text_to_gpt = []
     remaining_string_list0 = []
     remaining_string_list1 = []
     input_file = input_file
@@ -68,6 +96,7 @@ def vision_img(input_file):
     get_next_string = False
     for line in lines0:
         st.write(line)
+        text_to_gpt.append(line)
         for key, pattern in pattern_dict.items():
             matched_string = get_matched_string(pattern, line)
             if matched_string:
@@ -112,23 +141,26 @@ def vision_img(input_file):
         "人間合致判断の内容"  # この部分は `df_filtered.loc[row, "↓人間合致判断"]` の代わりに適切な値を入力してください
     ])
 
-    return [remaining_string_list0_record, remaining_string_list1_record]
+    return text_to_gpt
 
 input_file = st.file_uploader("画像をアップロードしてください", type=['jpg', 'jpeg'])
 
 if input_file:
     st.image(input_file)
-    record = vision_img(input_file)
+    text = vision_img(input_file)
 
-    remaining_string_list0_record, remaining_string_list1_record = record[0], record[1]
-
-    if record:
-        st.write([
-            remaining_string_list0_record[0], 
-            remaining_string_list0_record[1], 
-            remaining_string_list0_record[2], 
-            remaining_string_list1_record[0],
-            remaining_string_list1_record[1],
-            remaining_string_list1_record[2],
-            "人間合致判断の内容"  # この部分は `df_filtered.loc[row, "↓人間合致判断"]` の代わりに適切な値を入力してください
-        ])
+    if text:
+        client_gpt = OpenAI(api_key= API_KEY)
+        print(text)
+        st.write("---")
+        # プロンプトは適当
+        res = client_gpt.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": 
+             "以下のの文章から以下の情報を抽出して、表にしてください。\n・物件名称\n・タイプ間取\n・賃貸条件(/月)・管理費共益費・敷金・礼金・物件所在地・交通・建物(構造・規模)・建物(専有面積)・竣工年数・入居予定日・案内可能日・契約期間・更新料・駐車場・駐車場料金・賃貸保証委託契約・保険・管理会社・その他費用・設備"},
+            {"role": "user", "content": str(text)}
+        ]
+        )
+        st.write(f"以下情報({model_num})")
+        st.write(res.choices[0].message.content)
